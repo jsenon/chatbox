533c199790ed63df5ed38a1c3b7232a95f24cd4e
Julien Senon <julien.senon@gmail.com>
2017-03-08 11:18:06 +0100

    redis connexion

--- src/chatbox.go
+++ src/chatbox.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 )
 
+// TO DO
+// Build Docker File
+// Construct and configure Redis
+//
+
+// TO FIX
+
 func main() {
+
+	//Connect to Redis
+	c, err := redis.Dial("tcp", "127.0.0.1:6379")
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := c.Do("AUTH", "YOUR_PASSWORD")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Connected! ", response)
+	defer c.Close()
 
 }
