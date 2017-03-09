ef731848b6e4429b0948a347deac4165b1d32b78
Julien Senon <julien.senon@gmail.com>
2017-03-09 16:43:34 +0100

    Redis online/offline func

--- src/chatbox.go
+++ src/chatbox.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	// "os"
-	// "redis"
+	"bytes"
+	redigo "github.com/garyburd/redigo/redis"
+	"os"
+	"redis"
+	"time"
 	"webserver"
 )
 
@@ -18,33 +20,104 @@ import (
 
 // TO FIX
 
-var RedisServer = flag.String("ipaddr", "127.0.0.1", ":6379")
+var RedisServer bytes.Buffer
+
+// For testPurpose before retrieve it from webserver
+var username string
 
 func main() {
 
-	fmt.Println("connec chain", *RedisServer)
+	RedisServer.WriteString("127.0.0.1")
+	RedisServer.WriteString(":")
+	RedisServer.WriteString("6379")
+	// username = "John"
 
+	// TEST PURPOSE
+	// Username is retrieve through argument in command line
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: goredchat username")
+		os.Exit(1)
+	}
+	username := os.Args[1]
+
+	fmt.Println("connec chain", RedisServer.String())
+
+	// Call Redis connection
+	stateRedis, c, errRedis := redis.ConnectRedis(RedisServer.String())
+	if errRedis != nil {
+		panic(errRedis)
+	}
+	fmt.Println("status redis: ", stateRedis)
+	// Create a key
+	userkey := "online." + username
+	// EX seconds -- Set the specified expire time, in seconds.
+	// PX milliseconds -- Set the specified expire time, in milliseconds.
+	// NX -- Only set the key if it does not already exist.
+	// XX -- Only set the key if it already exist.
+	// We set a key online.John with user John only if key is not present and set expiration to 30s
+	val, err := c.Do("SET", userkey, username, "NX", "EX", "30")
+	// Handle Error
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	// Handle Key already present, username already online
+	if val == nil {
+		fmt.Println("User already online")
+		// To be redirected on future login page
+		panic(val)
+	}
+	fmt.Println("Log redis", c)
+	// We add the specified members to the set stored at key users
+	val, err = c.Do("SADD", "users", username)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	// Handle user already member
+	if val == nil {
+		fmt.Println("User already stored")
+		panic(val)
+	}
+	// Print who is connected
+	duration := time.Duration(2) * time.Second
+	// TEST PURPOSE
+	// Check online/offline function
+	for i := 0; i < 30; i++ {
+		// Force John lgout
+		if username == "John" && i == 15 {
+			c.Do("DEL", userkey)
+			c.Do("SREM", "users", username)
+			c.Do("PUBLISH", "messages", username+" has left")
+			os.Exit(1)
+		}
+		// Who is online
+		fmt.Println("-----")
+		names, _ := redigo.Strings(c.Do("SMEMBERS", "users"))
+		for _, name := range names {
+			fmt.Println("Users online: ", name)
+		}
+		time.Sleep(duration)
+	}
+	// Logout User
+	// Clean all keys
+	// Publish left message
+	c.Do("DEL", userkey)
+	c.Do("SREM", "users", username)
+	c.Do("PUBLISH", "messages", username+" has left")
+	// TEST PURPOSE ONLY
+	// A ticker will let us update our presence on the Redis server
+	// tickerChan := time.NewTicker(time.Second * 60).C
+
+	// Web Part
+	//Handle Func
 	http.HandleFunc("/login", webserver.Login)
 	http.HandleFunc("/mychat", webserver.Index)
 	http.HandleFunc("/room", webserver.Room)
-
 	//Handle URL ERROR
 	http.HandleFunc("/", webserver.Error)
 	// Init WebServer
-	if err := http.ListenAndServe(":10000", nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
-
-	// Call Redis connection
-	fmt.Println("connec chain", RedisServer)
-	//c, err := redis.ConnectRedis(RedisServer)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println("status redis: ", c)
-
-	// username.Name = req.FormValue("name")
-	// userkey := "online." + username.Name
-	// val, err := c.Do("SET", userkey, username.Name, "NX", "EX", "120")
-
+	// if err := http.ListenAndServe(":10000", nil); err != nil {
+	// 	log.Fatal("ListenAndServe:", err)
+	// }
 }
--- src/redis/redis.go
+++ src/redis/redis.go
@@ -1,21 +1,21 @@
 package redis
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/garyburd/redigo/redis"
 )
 
-func ConnectRedis(addr string) (state string) {
+func ConnectRedis(addr string) (state string, c redis.Conn, err error) {
 	//Connect to Redis
 
-	c, err := redis.Dial("tcp", addr)
+	c, err = redis.Dial("tcp", addr)
 	if err != nil {
 		panic(err)
 	}
 	//Close if something went wrong
-	defer c.Close()
+	//defer c.Close()
 	state = "Connected"
 	//username come from form login.html
-	fmt.Printf("Connected! ", c)
-	return state
+	//fmt.Printf("Connected! ", c)
+	return
 }
